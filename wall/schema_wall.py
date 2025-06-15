6fe183a61cd3ab9671e7a9d0826c02e6128c633e
waikit <waikit6461@gmail.com>
2025-06-15 17:25:57 +0800

    fixed wall

--- wall/schema_wall.py
+++ wall/schema_wall.py
@@ -1,15 +1,16 @@
 from datetime import datetime
 from pydantic import BaseModel
+from uuid import UUID
 
 
 class WallBase(BaseModel):
-    username: str
     input_text: str
 
     class Config:
         from_attributes = True
         
 class WallDisplay(BaseModel):
+    id: UUID
     created_at: datetime
     username: str
     input_text: str
